576ec44ab93febbf3caa8c001c5ae51ae89a4a69
Christopher Chen <christopher.chen1995@gmail.com>
2018-02-07 00:03:45 -0800

    Update url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     author="Christopher Chen",
     author_email="christopher.chen1995@gmail.com",
-    url="https://github.com/TheCDC/evil-rock-paper-scissors",
+    url="https://github.com/TheCDC/evil_rock_paper_scissors",
     install_requires=["pymarkoff"],
     packages=['evil_rock_paper_scisors']
 )
